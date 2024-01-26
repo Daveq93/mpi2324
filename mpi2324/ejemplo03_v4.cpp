3833e30f949e2e3a4cdccf920309562354fbf5b7
David Quizhpe <cdavidq.22@gmail.com>
2024-01-26 13:42:18 +0000

    v3-v4

---
+++ mpi2324/ejemplo03_v4.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<mpi.h>
+#include <cmath>
+#include<vector>
+
+#define MAX_ITEMS 25
+
+int sumar(int *tmp, int n) {
+    int suma = 0;
+    for (int i = 0; i < n; i++) {
+        suma = suma+tmp[i];
+    }
+    return suma;
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    int block_size;
+    int real_size;
+    int padding = 0;
+
+    if (MAX_ITEMS % nprocs != 0) {
+        real_size = std::ceil((double) MAX_ITEMS / nprocs) * nprocs;
+        block_size = real_size/nprocs;
+        padding = real_size - MAX_ITEMS;
+    }
+
+    std::vector<int> data;
+    std::vector<int> data_local;
+    int suma_total = 0;
+
+    if(rank==0)
+    {
+        int suma_local=0;
+        std::printf("Dimension: %d, real_size: %d, block_size: %d, padding: %d\n", MAX_ITEMS, real_size,
+                    block_size, padding);
+        //inicializar
+        for(int i=0;i<MAX_ITEMS;i++){
+            data[i] = i;
+        }
+
+
+        MPI_Scatter(data.data(),block_size,MPI_INT, //encio
+                    data_local.data(),block_size,MPI_INT,//que envia
+                    0,MPI_COMM_WORLD); //que rank
+
+        suma_local = sumar(data_local.data(),block_size);
+
+        MPI_Gather(MPI_IN_PLACE, 0, MPI_INT,
+                   data_local.data(), block_size, MPI_INT,
+                   0, MPI_COMM_WORLD);
+
+    }else{
+        //calcular la suma parcial
+        if(rank==nprocs-1){
+            block_size = block_size-padding;
+        }
+
+        int suma_parcial = sumar(data_local.data(),block_size);
+        std::printf("RANK_%d: suma parcial= %d\n",rank,suma_parcial);
+
+        //enviar la suma parcial al Rank_0
+
+        MPI_Reduce(&suma_parcial,&suma_total,1,MPI_INT,
+                   MPI_SUM,0,MPI_COMM_WORLD);
+
+    }
+    //imprimir el resultado
+    if(rank==0){
+        std::printf("SUMA TOTAL: %d\n",suma_total);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
--- mpi2324/ejemplo_matrices.cpp
+++ mpi2324/ejemplo_matrices.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<mpi.h>
 #include <cmath>
@@ -7,13 +8,15 @@
 #define MATRIX_DIMENSION 25
 
 void matrix_mult(double *A, double *b, double *c, int rows, int cols) {
-    for (int i = 0; i < rows; ++i) {
-        double tmp;
-        for (int j = 0; j < cols; ++j) {
+    for (int i = 0; i < rows; i++) {
+        double tmp = 0;  // Inicializar tmp a cero
+        for (int j = 0; j < cols; j++) {
             tmp = tmp + A[i * cols + j] * b[j];
         }
+        c[i] = tmp;
     }
 }
+
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -45,13 +48,12 @@ int main(int argc, char **argv) {
         std::vector<double> c(rows_alloc);
 
         for (int i = 0; i < MATRIX_DIMENSION * MATRIX_DIMENSION; i++) {
-            for (int j = 0; MATRIX_DIMENSION; j++) {
+            for (int j = 0; j < MATRIX_DIMENSION; j++) {
                 int index = i * MATRIX_DIMENSION + j;
                 A[index] = i;
             }
+        }
 
-
-        };
         for (int i = 0; i < MATRIX_DIMENSION; i++)b[i] = 1;
 
         //Enviar la matriz A
@@ -77,7 +79,6 @@ int main(int argc, char **argv) {
         for (int i = 0; i < MATRIX_DIMENSION; i++) {
             std::printf("%.0f, ", c[i]);
         }
-
     } else {
         std::vector<double> A_local(MATRIX_DIMENSION * rows_per_rank);
         std::vector<double> b_local(MATRIX_DIMENSION);
@@ -113,3 +114,117 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+*/
+
+#include<iostream>
+#include<mpi.h>
+#include <cmath>
+#include<vector>
+
+#define MATRIX_DIMENSION 25
+
+void matrix_mult(double *A, double *b, double *c, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        double tmp = 0;  // Inicializar tmp a cero
+        for (int j = 0; j < cols; j++) {
+            tmp = tmp + A[i * cols + j] * b[j];
+        }
+        c[i] = tmp;
+    }
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, nprocs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    int rows_per_rank;
+    int rows_alloc = MATRIX_DIMENSION;
+    int padding = 0;
+
+    if (MATRIX_DIMENSION % nprocs != 0) {
+        rows_alloc = std::ceil((double) MATRIX_DIMENSION / nprocs) * nprocs;
+        padding = rows_alloc - MATRIX_DIMENSION;
+    }
+
+    rows_per_rank = rows_alloc / nprocs;
+
+    std::vector<double> c(rows_alloc);
+
+    if (rank == 0) {
+        std::printf("Dimension: %d, rows_alloc: %d, rows_per_rank: %d, padding: %d\n", MATRIX_DIMENSION, rows_alloc,
+                    rows_per_rank, padding);
+
+        std::vector<double> A(MATRIX_DIMENSION * rows_alloc);
+        std::vector<double> b(MATRIX_DIMENSION);
+
+        for (int i = 0; i < MATRIX_DIMENSION * MATRIX_DIMENSION; i++) {
+            for (int j = 0; j < MATRIX_DIMENSION; j++) {
+                int index = i * MATRIX_DIMENSION + j;
+                A[index] = i;
+            }
+        }
+
+        for (int i = 0; i < MATRIX_DIMENSION; i++) b[i] = 1;
+
+        // Enviar la matriz A
+        MPI_Scatter(A.data(), MATRIX_DIMENSION * rows_per_rank, MPI_DOUBLE,
+                    MPI_IN_PLACE, 0, MPI_DOUBLE,
+                    0, MPI_COMM_WORLD);
+
+        // Enviar el vector b
+        MPI_Bcast(b.data(), MATRIX_DIMENSION, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        // Realizar el cálculo: c = A*b;
+        matrix_mult(A.data(), b.data(), c.data(), rows_per_rank, MATRIX_DIMENSION);
+
+        // Recibir los resultados
+        MPI_Gather(MPI_IN_PLACE, 0, MPI_DOUBLE,
+                   c.data(), rows_per_rank, MPI_DOUBLE,
+                   0, MPI_COMM_WORLD);
+
+        c.resize(MATRIX_DIMENSION);
+
+        // Imprimir el resultado
+        std::printf("Resultado\n");
+        for (int i = 0; i < MATRIX_DIMENSION; i++) {
+            std::printf("%.0f, ", c[i]);
+        }
+    } else {
+        std::vector<double> A_local(MATRIX_DIMENSION * rows_per_rank);
+        std::vector<double> b_local(MATRIX_DIMENSION);
+
+        // Recibir matriz A
+        MPI_Scatter(nullptr, 0, MPI_DOUBLE,
+                    A_local.data(), MATRIX_DIMENSION * rows_per_rank, MPI_DOUBLE,
+                    0, MPI_COMM_WORLD);
+
+        std::printf("RANK_%d: [%.0f..%.0f] \n", rank, A_local[0], A_local.back());
+
+        // Recibir en vector b
+        MPI_Bcast(b_local.data(), MATRIX_DIMENSION, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        // Realizar el cálculo: c = A*b;
+        int rows_per_rank_tmp;
+
+        if (rank == nprocs - 1) {
+            rows_per_rank_tmp = MATRIX_DIMENSION - rank * rows_per_rank;
+        }
+
+        matrix_mult(A_local.data(), b_local.data(), c.data(), rows_per_rank_tmp, MATRIX_DIMENSION);
+
+        // Enviar el resultado parcial
+        MPI_Gather(c.data(), rows_per_rank, MPI_DOUBLE,
+                   nullptr, 0, MPI_DOUBLE,
+                   0, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+
+
